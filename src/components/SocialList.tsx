68485e2469df0613cc1f0d5d037ef5b279578f18
Michael Miller <mjmill22@asu.edu>
2021-06-04 14:36:08 -0700

    fix: remove twitter from social list

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -6,14 +6,6 @@ import config from '../lib/config';
 const SocialList = () => (
   <div>
     <a
-      title="Twitter"
-      href={`https://twitter.com/${config.twitter_account}`}
-      target="_blank"
-      rel="noopener"
-    >
-      <Twitter width={24} height={24} fill={'#222'} />
-    </a>
-    <a
       title="GitHub"
       href={`https://github.com/${config.github_account}`}
       target="_blank"
